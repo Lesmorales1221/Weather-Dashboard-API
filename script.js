c937bbd21adc1a68afea2865e8b2d91b5adc68bd
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-07-14 23:06:42 -0400

    added a display weather function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let weather = {
     apiKey:"4fcf46f041c775cd5f30267cce8d41cb    ",
-    fetchWeather: function () {
+    fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
         ) .then((response)=> response.json())
             .then((data)=> console.log(data));
     },
+    displayWeather: function(data){
+
+    }
+
+
 };
