71180cee7c0bb14509ef47405a56ede665bee18d
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-07-14 23:18:49 -0400

    added const for the function display weather

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let weather = {
             .then((data)=> console.log(data));
     },
     displayWeather: function(data){
-
+        const {name} = data;
+        const {icon, description} = data.weather [0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
     }
 
 
