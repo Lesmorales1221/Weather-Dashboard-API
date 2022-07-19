8b6b92de322800bd26f9a1fe8bcb3cbeba55581b
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-07-18 22:15:18 -0400

    added query selector for the city description and temp in script.js

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ let weather = {
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
         ) .then((response)=> response.json())
-            .then((data)=> console.log(data));
+            .then((data)=> this.displayWeather(data));
     },
     displayWeather: function(data){
         const {name} = data;
         const {icon, description} = data.weather [0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in" + name;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp")
+    
     }
 
 
