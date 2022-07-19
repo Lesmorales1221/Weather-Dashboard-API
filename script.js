59b04918450b9a9d505870d4b83c62162ddc8e82
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-07-18 23:02:46 -0400

    added the remaining doc query selector for the info in the weather box

--- script.js
+++ script.js
@@ -10,12 +10,24 @@ let weather = {
         const {icon, description} = data.weather [0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
         document.querySelector(".city").innerText = "Weather in" + name;
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp")
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed:" + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading")
     
-    }
+    },
 
+    search:function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 
 };
+
+document.querySelector("search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document
+.querySelector(".search-bar")
